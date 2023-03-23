2cda1e0e6ec94ae1684718ea912382899e703dc8
Song shuhan <slof-songshuhan@163.com>
2023-03-24 03:06:12 +0800

    revise build easydict 🍈

--- Train_Runner.py
+++ Train_Runner.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     parser.add_argument('--attack',         type=str,               default=  'no') # ['DICE-0.1','Meta_Self-0.05' ,...] 攻击方式-扰动率
     parser.add_argument('--gpu_id',         type=int,               default=  1) 
     parser.add_argument('--model_name',     type = str,             default = 'MaskGAE')
+    parser.add_argument('--task',           type = str,             default = 'node') # 'link' 'graph'
     margs = parser.parse_args()
 
     if margs.model_name == 'GRACE':
@@ -18,5 +19,10 @@ if __name__ == '__main__':
         Train_MaskGAE_nodecls(margs)
     elif margs.model_name == 'GraphMAE':
         Train_GraphMAE_nodecls(margs)
+    elif margs.model_name == 'VGAE':
+        if margs.task == 'link':
+            Train_VGAE_linkcls(margs)
+        elif margs.task == 'node':
+            Train_VGAE_nodecls(margs)
 
     
--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 python Train_Runner.py \
 --dataset  'Attack-Cora' \
---attack  'Meta_Self-0.05' \
+--attack  'Meta_Self-0.0' \
+--task 'node' \
 --gpu_id 0 \
---model_name 'GraphMAE'
+--model_name 'VGAE'
